154c03b7aec7314c80e10b61dab872065934c54e
mallampati <coresoft.srinivas@gmail.com>
2020-09-03 00:51:37 +0530

    Create Clock.java

---
+++ Clock.java
@@ -0,0 +1,15 @@
+public class Clock{
+  private int hours;
+  private int minutes;
+  private int seconds;
+  
+  public Clock(){
+    this.hours = hours;
+    this.minutes = minutes;
+    this.seconds = seconds;
+  }
+  
+  public void showTime(){
+    System.out.println(hours+":"+minutes+":"+seconds);
+  }
+}
