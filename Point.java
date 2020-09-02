b476ebd1914fdb461a03cb921e06b96178889664
Srinivas Mallampati <coresoft.srinivas@gmail.com>
2020-09-03 04:13:19 +0530

    point.java modified and commited

--- Point.java
+++ Point.java
@@ -1,3 +1,13 @@
 public class Point {
-    
+    private int x;
+    private int y;
+
+    public Point(){
+        this.x=0;
+        this.y=0;
+    }
+
+    public void showPoint(){
+        System.out.println(x,y);
+    }
 }
