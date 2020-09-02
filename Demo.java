b684e4a709029da7971b82781898eca30b72b0a2
Srinivas Mallampati <coresoft.srinivas@gmail.com>
2020-09-03 00:44:54 +0530

    Demo.java added

---
+++ Demo.java
@@ -0,0 +1,8 @@
+import java.util.logging.Logger;
+
+public class Demo {
+    static Logger logger = Logger.getLogger("myproj");
+    public static void main(String args[]) throws java.lang.Exception {
+        logger.info("Hello World!!!");
+    }
+}
